27117ea24bbe6f88e77cae4f0a37cecee552ddc0
Changsik Kim <113814195+KCS2183@users.noreply.github.com>
2025-07-02 18:58:53 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+output = sys.stdout.write
+
+n = int(input())
+dp = [0, 1, 3] + [0] * (n-2)
+
+for i in range(3, n+1):
+    dp[i] = ((2 * dp[i-2]) + dp[i-1]) % 10007
+
+output(str(dp[n]))
